d831a202a72e17d99617cb224d6ab07ce81815ba
Szymon <32157216+Zawartas@users.noreply.github.com>
2018-03-05 20:12:54 +0100

    Add files via upload

---
+++ lotto.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    srand(time(nullptr));
+    std::set<int> zbior;
+    while (zbior.size() != 6){
+        zbior.insert(rand()%49+1);
+    }
+
+    for(auto &it: zbior)
+        std::cout << it << " ";
+
+    return 0;
+}
